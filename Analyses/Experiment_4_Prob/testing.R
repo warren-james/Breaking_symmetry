d5b71e5793f54e27614b2c38a7dcc4dee10e95b5
warren-james <w.james@abdn.ac.uk>
2019-11-05 16:43:46 +0000

    Sorted out acc measures

--- Analyses/Experiment_4_Prob/testing.R
+++ Analyses/Experiment_4_Prob/testing.R
@@ -1,3 +1,6 @@
+# library 
+library(tidyverse)
+
 # work out opt values 
 load("scratch/new_data/acc_sep")
 load("scratch/new_data/df_part2")
@@ -13,7 +16,7 @@ c_dist <- df_part2 %>%
   distinct(separation)
 
 # get acc for this strat
-c_acc <- merge(dist, acc_sep) %>% 
+c_acc <- merge(c_dist, acc_sep) %>% 
   mutate(acc_type = "Centre")
 
 # side strat version 
@@ -34,6 +37,85 @@ ll_acc <- acc_sep %>%
          -accuracy)
 
 
-test <- merge(s_dist, ml_acc) %>% 
+s_acc <- merge(s_dist, ml_acc) %>% 
   merge(ll_acc) %>% 
-  mutate(acc_type = "Side")
+  mutate(acc_type = "Side",
+         accuracy = (ML_acc * max_chance) + (LL_acc * (1 - max_chance))) %>% 
+  select(separation, participant, bias_type, max_chance, accuracy, acc_type)
+
+# bind together 
+acc_sep2 <- rbind(s_acc, c_acc) %>% 
+  spread(acc_type, accuracy) %>% 
+  mutate(opt_side = ifelse(Side > Centre, 1, 0))
+
+# need to do the above on all accuracy data to look at the switch point
+acc_c <- acc_sep %>% 
+  rbind(acc_sep) %>%
+  mutate(acc_type = "Centre") %>%
+  mutate(bias_type = rep(c("symmetric", "bias"), each = length(acc_sep$separation)))
+
+acc_ml <- acc_sep %>% 
+  mutate(ML_dist = separation,
+         ML_acc = accuracy) %>% 
+  select(-separation,
+         -accuracy)
+acc_ll <- acc_sep %>% 
+  mutate(LL_dist = separation,
+         LL_acc = accuracy) %>% 
+  select(-separation,
+         -accuracy)
+
+acc_s <- acc_sep %>% 
+  mutate(ML_dist = 1,
+         LL_dist = separation * 2) %>%
+  merge(acc_ml) %>% 
+  merge(acc_ll) %>% 
+  mutate(accuracy_bias = (ML_acc * 0.8) + (LL_acc * 0.2),
+         accuracy_symmetric = (ML_acc * 0.5) + (LL_acc * 0.5)) %>% 
+  # select(participant, separation, accuracy_bias, accuracy_symmetric) %>% 
+  gather(c(accuracy_bias, accuracy_symmetric),
+         key = "bias_type", 
+         value = "accuracy") %>% 
+  separate(bias_type, 
+           c("remove", "bias_type"),
+           sep = "_") %>% 
+  mutate(acc_type = "Side") %>% 
+  select(participant, separation, accuracy, acc_type, bias_type)
+
+
+# all together
+acc_all <- rbind(acc_c, acc_s) %>% 
+  spread(acc_type, accuracy) %>% 
+  drop_na() %>% 
+  mutate(opt_side = ifelse(Centre > Side, 0, 1))
+
+# do some checks for each participant 
+checking <- acc_all %>% 
+  group_by(participant, bias_type) %>% 
+  mutate(n = n()) %>% 
+  summarise(prop = sum(opt_side)/unique(n))
+
+# plot to have a look 
+checking %>% 
+  ggplot(aes(participant, prop,
+             fill = bias_type)) + 
+  geom_bar(stat = "identity",
+           position = "dodge") + 
+  see::scale_fill_flat() + 
+  scale_y_continuous("Proportion of Distances participant should fixate the side box")
+  
+# plot diff in strat over distance 
+acc_all %>% 
+  mutate(diff = Side - Centre) %>% 
+  ggplot(aes(separation, diff, colour = bias_type)) + 
+  geom_line() + 
+  facet_wrap(~participant) + 
+  # see::scale_color_flat() + 
+  scale_colour_viridis_d() + 
+  geom_hline(yintercept = 0,
+             linetype = "dashed",
+             colour = "white") + 
+  scale_y_continuous("Side - Centre") + 
+  see::theme_blackboard() + 
+  theme(strip.text.x = element_text(colour = "white"))
+   
