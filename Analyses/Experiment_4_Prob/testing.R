ae1a33249726d5619c755455e3f943b1c5aa1421
warren-james <w.james@abdn.ac.uk>
2019-11-06 15:07:49 +0000

    New plot added

--- Analyses/Experiment_4_Prob/testing.R
+++ Analyses/Experiment_4_Prob/testing.R
@@ -4,6 +4,7 @@ library(tidyverse)
 # work out opt values 
 load("scratch/new_data/acc_sep")
 load("scratch/new_data/df_part2")
+load("scratch/new_data/AccMea")
 
 # quick pre-process 
 df_part2 <- df_part2 %>% 
@@ -110,12 +111,118 @@ acc_all %>%
   ggplot(aes(separation, diff, colour = bias_type)) + 
   geom_line() + 
   facet_wrap(~participant) + 
-  # see::scale_color_flat() + 
-  scale_colour_viridis_d() + 
+  see::scale_color_flat() +
   geom_hline(yintercept = 0,
              linetype = "dashed",
              colour = "white") + 
-  scale_y_continuous("Side - Centre") + 
-  see::theme_blackboard() + 
-  theme(strip.text.x = element_text(colour = "white"))
+  scale_y_continuous("Side - Centre")
    
+#### Sort out Expected and Optimal accuracy ####
+acc_opt <- acc_all %>% 
+  mutate(Optimal = pmax(Centre, Side)) %>% 
+  select(participant, separation, Optimal) %>% 
+  distinct()
+
+r_acc <- acc_c %>%
+  mutate(r_dist = separation,
+         r_acc = accuracy) %>% 
+  select(participant, r_dist, r_acc) %>% 
+  distinct()
+l_acc <- acc_c %>%
+  mutate(l_dist = separation,
+         l_acc = accuracy) %>% 
+  select(participant, l_dist, l_acc) %>%
+  distinct()
+c_acc <- acc_c %>% 
+  mutate(c_dist = separation, 
+         c_acc = accuracy) %>% 
+  select(participant, c_dist, c_acc) %>% 
+  distinct()
+fix_acc <- acc_c %>% 
+  mutate(fix_dist = separation,
+         fix_acc = accuracy) %>% 
+  select(participant, fix_dist, fix_acc) %>% 
+  distinct()
+far_acc <- acc_c %>% 
+  mutate(far_dist = separation,
+         far_acc = accuracy) %>%
+  select(participant, far_dist, far_acc) %>% 
+  distinct()
+
+test <- df_part2 %>% 
+  select(participant, bias_type, bias_left, separation, fixated_box, accuracy) %>% 
+  mutate(l_dist = ifelse(fixated_box == 1, separation, 
+                         ifelse(fixated_box == 2, 1, 2*separation)),
+         r_dist = ifelse(fixated_box == 1, separation, 
+                         ifelse(fixated_box == 3, 1, 2*separation)),
+         c_dist = separation,
+         fix_dist = 1,
+         far_dist = separation * 2,
+         ll_bias = 1 - bias_left) %>% 
+  merge(l_acc) %>% 
+  merge(r_acc) %>% 
+  merge(c_acc) %>%
+  merge(far_acc) %>%
+  merge(fix_acc) %>% 
+  mutate(Expected = (l_acc * bias_left) + (r_acc * ll_bias),
+         Centre = (c_acc * bias_left) + (c_acc * ll_bias),
+         Side = (fix_acc * pmax(bias_left, ll_bias)) + (far_acc * (pmin(bias_left, ll_bias))),
+         Optimal = pmax(Side, Centre))%>% 
+  select(participant, 
+         separation, 
+         bias_type, 
+         accuracy, 
+         Expected,
+         Centre,
+         Side,
+         Optimal)
+
+# overall plot
+plt_check <- test %>% 
+  group_by(participant, bias_type, separation) %>% 
+  summarise(Actual = mean(accuracy),
+            Centre = mean(Centre),
+            Side = mean(Side),
+            Expected = mean(Expected),
+            Optimal = mean(Optimal)) %>% 
+  ungroup() %>% 
+  mutate(diff_EO = Expected/Optimal,
+         diff_CO = Centre/Optimal,
+         participant = as.factor(as.numeric(participant))) %>% 
+  ggplot(aes(separation, diff_EO, colour = bias_type)) + 
+  geom_line() + 
+  see::scale_colour_flat() + 
+  facet_wrap(~participant) + 
+  theme_bw() + 
+  geom_line(aes(separation, diff_CO, colour = bias_type),
+            linetype = "dashed") + 
+  scale_y_continuous("Expected/Optimal") + 
+  scale_x_continuous("Delta (pixels)")
+plt_check
+
+# now take the above data and we can plot something like the boxplots from before 
+plt_box_acc <- test %>% 
+  mutate(dist_type = ifelse(Centre > Side, "close", "far")) %>% 
+  group_by(participant, bias_type, dist_type) %>% 
+  summarise(Actual = mean(accuracy),
+            Centre = mean(Centre),
+            Side = mean(Side),
+            Expected = mean(Expected),
+            Optimal = mean(Optimal)) %>% 
+  ungroup() %>% 
+  mutate(diff_EO = Expected/Optimal,
+         diff_CO = Centre/Optimal,
+         participant = as.factor(as.numeric(participant))) %>% 
+  ggplot(aes(dist_type, diff_EO,
+             colour = bias_type,
+             fill = bias_type)) + 
+  geom_boxplot(alpha = .3) + 
+  see::scale_color_flat() + 
+  see::scale_fill_flat() + 
+  scale_y_continuous("Expected/Optimal") + 
+  scale_x_discrete("Distance Type")
+plt_box_acc
+
+
+
+
