497f4091425e6aaea22478bf8fe6e87f20773f8d
warren-james <w.james@abdn.ac.uk>
2019-11-28 18:40:12 +0000

    New model added
    Model of side fixations
    Had to use Bayesian as glmer would not converge

--- Analyses/Experiment_4_Prob/6_analysis_paper.R
+++ Analyses/Experiment_4_Prob/6_analysis_paper.R
@@ -7,6 +7,7 @@ library(lme4)
 library(brms)
 
 #### functions ####
+# should I write a function for plotting predictions?
 
 #### load data ####
 load("scratch/new_data/df_part2_fixed")
@@ -16,8 +17,7 @@ df_model <- df_part2_fixed %>%
   filter(separation != 640) %>% # remove furthest point for now
   select(participant, dist_type, bias_type, separation, st_box, accuracy) %>% 
   mutate(Ml_fix = ifelse(st_box == "most likely", 1, 0),
-         C_fix = ifelse(st_box == "centre", 1, 0),
-         ) # rescale separation to avoid 
+         S_fix = ifelse(st_box != "centre", 1, 0)) # rescale separation to avoid 
 
 #### analysis ####
 #### Frequentist ####
@@ -50,8 +50,8 @@ m_fix_like_sep.3 <- glmer(Ml_fix ~ (bias_type + dist_type)^2 +
                           family = "binomial")
 
 # plot this? 
-df_model$p = predict(m_fix_like_sep.3, type = "response")
-df_model$p_fe = predict(m_fix_like_sep.3, re.form = NA, type = "response")
+df_model$p = predict(m_fix_like_sep.2, type = "response")
+df_model$p_fe = predict(m_fix_like_sep.2, re.form = NA, type = "response")
 df_model %>% 
   mutate(participant = as.factor(as.numeric(participant))) %>%
   group_by(participant, bias_type, dist_type) %>% 
@@ -98,7 +98,15 @@ df_model %>%
   see::scale_color_flat() + 
   see::scale_fill_flat() 
 
-
+#### Fixation Side ####
+# full model like the above 
+# fails to converge... try bayes for now 
+m_fix_S_sep.2 <- glmer(S_fix ~ (bias_type + dist_type)^2 +
+                         (bias_type - 1|participant) + 
+                         (dist_type - 1|participant) + 
+                         (1|participant),
+                       data = df_model,
+                       family = "binomial")
 
 #### Accuracy ####
 # Is this needed really?
@@ -112,5 +120,35 @@ bm_fix_like_dt <- brm(Ml_fix ~ (bias_type + dist_type)^2 + (dist_type + bias_typ
                       chains = 1,
                       iter = 1000,
                       warmup = 500)
+
+#### Side ####
+bm_fix_S_dt <- brm(S_fix ~ (bias_type + dist_type)^2 + (dist_type + bias_type|participant), 
+                   data = df_model,
+                   family = "bernoulli",
+                   chains = 1,
+                   iter = 1000,
+                   warmup = 500)
+
+df_model$p_b <- predict(bm_fix_S_dt, type = "response")[,1]
+df_model %>% 
+  group_by(participant, dist_type, bias_type) %>%
+  summarise(Actual = mean(S_fix),
+            Predicted = mean(p_b)) %>% 
+  ggplot(aes(participant, Proportion,
+             colour = dist_type,
+             fill = dist_type)) +
+  geom_point(aes(y = Actual),
+             shape = 21) +
+  geom_point(aes(y = Predicted),
+             fill = "white",
+             shape = 21) +
+  # geom_hline(aes(yintercept = FE,
+  #                colour = dist_type),
+  #            linetype = "dashed") +
+  facet_wrap(~bias_type) + 
+  see::scale_color_flat() +
+  see::scale_fill_flat() +
+  theme_bw()
+  
 
 #### Accuracy ####
