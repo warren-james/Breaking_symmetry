a1661c75356a53860b2f6a513596a71e0fc19e8c
warren-james <w.james@abdn.ac.uk>
2019-11-28 16:41:29 +0000

    Models fit to data
    Model for Most likely box fixations sorted. Plots of fit made

--- Analyses/Experiment_4_Prob/6_analysis_paper.R
+++ Analyses/Experiment_4_Prob/6_analysis_paper.R
@@ -21,23 +21,90 @@ df_model <- df_part2_fixed %>%
 
 #### analysis ####
 #### Frequentist ####
+#### Fixation to Ml side ####
 # likely fixation ~ bias_type
 m_fix_like <- glmer(Ml_fix ~ bias_type + (bias_type|participant), 
                     data = df_model,
                     family = "binomial")
 
 # add in separation? 
-m_fix_like_sep <- glmer(Ml_fix ~ (bias_type + dist_type)^2 + (dist_type + bias_type|participant), 
+m_fix_like_sep <- glmer(Ml_fix ~ (bias_type + dist_type)^2 + (1|participant),
+                        data = df_model,
+                        family = "binomial")
+
+# add some random effects 
+m_fix_like_sep.1 <- glmer(Ml_fix ~ (bias_type + dist_type)^2 + (dist_type + bias_type|participant), 
                     data = df_model,
                     family = "binomial")
+# fully random 
+m_fix_like_sep.2 <- glmer(Ml_fix ~ (bias_type + dist_type)^2 + ((bias_type + dist_type)^2|participant),
+                          data = df_model,
+                          family = "binomial")
 
-# centre fixation ~ bias_type 
-m_fix_centre <- glmer(C_fix ~ bias_type + (1|participant),
-                      data = df_model,
-                      family = "binomial")
+# random: effects are independent 
+m_fix_like_sep.3 <- glmer(Ml_fix ~ (bias_type + dist_type)^2 +
+                            (1|participant) +
+                            (bias_type - 1|participant) +
+                            (dist_type - 1|participant),
+                          data = df_model, 
+                          family = "binomial")
+
+# plot this? 
+df_model$p = predict(m_fix_like_sep.3, type = "response")
+df_model$p_fe = predict(m_fix_like_sep.3, re.form = NA, type = "response")
+df_model %>% 
+  mutate(participant = as.factor(as.numeric(participant))) %>%
+  group_by(participant, bias_type, dist_type) %>% 
+  summarise(Predicted = mean(p),
+            Actual = mean(Ml_fix),
+            FE = mean(p_fe)) %>%
+  ungroup() %>% 
+  group_by(bias_type, dist_type) %>%
+  mutate(med_FE = median(Predicted)) %>%
+  ggplot(aes(participant, Proportion,
+             colour = dist_type,
+             fill = dist_type)) +
+  geom_point(aes(y = Actual),
+             shape = 21) +
+  geom_point(aes(y = Predicted),
+             fill = "white",
+             shape = 21) +
+  geom_hline(aes(yintercept = FE,
+                 colour = dist_type),
+             linetype = "dashed") +
+  facet_wrap(~bias_type) + 
+  see::scale_color_flat() +
+  see::scale_fill_flat() +
+  theme_bw()
+
+# need to extract fixed effects... not too hard 
+df_model$p_fe <- predict(m_fix_like_sep.2, re.form = NULL, type = "response")
+
+# an overall plot? 
+df_model %>% 
+  group_by(bias_type, dist_type, participant) %>% 
+  summarise(Predicted = mean(p),
+            Actual = mean(Ml_fix)) %>% 
+  gather(Predicted:Actual, 
+         key = "Type",
+         value = "Proportion") %>% 
+  ggplot(aes(dist_type, Proportion,
+             colour = Type,
+             fill = Type)) + 
+  geom_boxplot(alpha = .3) + 
+  facet_wrap(~bias_type) + 
+  theme_bw() + 
+  scale_y_continuous(breaks = seq(0,1,.2)) +
+  see::scale_color_flat() + 
+  see::scale_fill_flat() 
 
 
+
+#### Accuracy ####
+# Is this needed really?
+# Maybe we can do what we did with the penguin paper?
 #### Bayesian ####
+#### Ml side ####
 # most complex model we need?
 bm_fix_like_dt <- brm(Ml_fix ~ (bias_type + dist_type)^2 + (dist_type + bias_type|participant),
                       data = df_model,
@@ -45,10 +112,5 @@ bm_fix_like_dt <- brm(Ml_fix ~ (bias_type + dist_type)^2 + (dist_type + bias_typ
                       chains = 1,
                       iter = 1000,
                       warmup = 500)
-# let's remove random effects all together and see how it changes 
-test <- brm(Ml_fix ~ (bias_type + dist_type)^2,
-            data = df_model,
-            family = "bernoulli",
-            chains = 1,
-            iter = 1000,
-            warmup = 500)
+
+#### Accuracy ####
